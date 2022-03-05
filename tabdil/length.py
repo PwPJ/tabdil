8cf9a493261a2eb1c2d88ef8b2a6124a68e42d07
zs44 <samadi83@gmail.com>
2022-03-05 08:54:18 -1000

    add new file lengh weight

---
+++ tabdil/length.py
@@ -0,0 +1,20 @@
+
+
+
+# this function converts sort of length units (kilometers,metres,centimetre) to milligrams
+
+def millimeter(length_unit):
+	unit_name = length_unit[0].lower()
+	number = 1
+	if unit_name == 'km':
+		number = 1000 * 100 * 10 
+	elif unit_name == 'm':
+		number = 100 * 10 
+	elif unit_name == 'cm':
+		number = 10 
+	elif unit_name == 'mm':
+		pass
+	else:
+		raise ValueError('unknown unit name {!r}'.format(unit_name))
+	unit_number = length_unit[1]
+	return unit_number * number
