8e39541d58e7b9fb928e01b56414d0f80b3e3a25
Alireza Sadeghi <aliraza7926@gmail.com>
2022-03-07 13:34:14 +0330

    add gram convertor

--- tabdil/weight.py
+++ tabdil/weight.py
@@ -17,3 +17,21 @@ def milli_gram(weight_unit):
 		raise ValueError('unknown unit name {!r}'.format(unit_name))
 	unit_number = weight_unit[1]
 	return unit_number * number
+
+
+# this function converts sort of weight units (tons,kilograms,gerams) to grams
+def gram(weight_unit):
+	unit_name = weight_unit[0].lower()
+	number = 1
+	if unit_name == 'ton':
+		number = 1000 * 1000
+	elif unit_name == 'kg':
+		number = 1000 
+	elif unit_name == 'mg':
+		number = 0.001
+	elif unit_name == 'g':
+		pass
+	else:
+		raise ValueError('unknown unit name {!r}'.format(unit_name))
+	unit_number = weight_unit[1]
+	return unit_number * number
