00b9737c68b121df3ec63c98d3435e0ba230da67
kavehbarzegar <kavehbarzegar2020gmail.com>
2022-03-08 11:29:08 +0330

    On branch new_branch 	new file:   temperature.py

---
+++ tabdil/temperature.py
@@ -0,0 +1,19 @@
+
+
+def fahrenheit(tem_unit):
+    unit_tem = tem_unit[0].lower()
+    number = 1
+    unit_number = tem_unit[1]
+    if unit_tem == 'c':
+        number = unit_number * 1.8 +32
+    elif unit_tem == 'k':
+        number = unit_number * 1.8 -459.67
+    elif unit_tem == 'ra':
+        number = unit_number - 459.67
+    elif unit_tem == 're':
+        number = unit_number * 2.25 + 32
+    elif unit_tem == 'f':
+        pass
+    else:
+         raise ValueError('unknown unit name {!r}'.format(unit_name))
+    return number
