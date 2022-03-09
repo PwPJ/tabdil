e2813a5bc83fbafd32a6abdc437e64427eebf7b6
Hamid‌ Eghbali <daeehamid@gmail.com>
2022-03-10 00:02:52 +0330

    refactor time.py

--- tabdil/time.py
+++ tabdil/time.py
@@ -3,17 +3,16 @@
 def seconds(time_unit):
 	unit_name = time_unit[0].lower()
 	number = 1
-	if unit_name == 'w':
-		number = 7 * 24 * 60 * 60
-	elif unit_name == 'd':
-		number = 24 * 60 * 60
-	elif unit_name == 'h':
-		number = 60 * 60
-	elif unit_name == 'm':
-		number = 60
-	elif unit_name == 's':
-		pass
-	else:
-		raise ValueError('unknown unit name {!r}'.format(unit_name))
+	number = Switcher(unit_name)
 	unit_number = time_unit[1]
 	return unit_number * number
+
+
+def Switcher(unit):
+    return {
+        'w': 7 * 24 * 60 * 60,
+        'd': 24 * 60 * 60,
+        'h': 60 * 60,
+        'm': 60,
+		's': 1
+    }.get(unit, f'unknown unit name: {unit}!')
