b10e2629707df64a7d5c3ad4f3121eecd0c7f9b5
kavehbarzegar <kavehbarzegar2020gmail.com>
2022-03-09 18:07:04 +0330

    modified:   __init__.py 	modified:   temperature.py

--- tabdil/__init__.py
+++ tabdil/__init__.py
@@ -1,1 +1,2 @@
 from . import time
+from . import temperature
--- tabdil/temperature.py
+++ tabdil/temperature.py
@@ -4,6 +4,7 @@ def fahrenheit(tem_unit):
     unit_tem = tem_unit[0].lower()
     number = 1
     unit_number = tem_unit[1]
+    unit_name = tem_unit[0].lower()
     if unit_tem == 'c':
         number = unit_number * 1.8 +32
     elif unit_tem == 'k':
