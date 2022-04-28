3696c54c8f417558db3d77ef77dc8033115953f7
NikaGolestani <83786259+NikaGolestani@users.noreply.github.com>
2022-04-28 21:57:56 +0300

    add kilometre ,meter and inch tp lenght.py

--- tabdil/length.py
+++ tabdil/length.py
@@ -1,7 +1,7 @@
 
 
 
-# this function converts sort of length units (kilometers,metres,centimetre) to millimeters
+# this function converts sort of length units (kilometers,metres,centimetre,inch) to millimeters
 
 def millimeter(length_unit):
 	unit_name = length_unit[0].lower()
@@ -14,12 +14,14 @@ def millimeter(length_unit):
 		number = 10 
 	elif unit_name == 'mm':
 		pass
+	elif unit_name == 'inch':
+		number=25.4
 	else:
 		raise ValueError('unknown unit name {!r}'.format(unit_name))
 	unit_number = length_unit[1]
 	return unit_number * number
 
-# this function converts sort of length units (kilometers,metres,millimeter) to centimetre
+# this function converts sort of length units (kilometers,metres,millimeter,inch) to centimetre
 
 def centimeter(length_unit):
 	unit_name = length_unit[0].lower()
@@ -32,9 +34,69 @@ def centimeter(length_unit):
 		pass 
 	elif unit_name == 'mm':
 		number = 1/10
+	elif unit_name == 'inch':
+		number=2.54
+	else:
+		raise ValueError('unknown unit name {!r}'.format(unit_name))
+	unit_number = length_unit[1]
+	return unit_number * number	
+
+# this function converts sort of length units (kilometers,millimetres,centimetre,inch) to meter
+
+def meter(length_unit):
+	unit_name = length_unit[0].lower()
+	number = 1
+	if unit_name == 'km':
+		number = 1000 
+	elif unit_name == 'm':
+		pass
+	elif unit_name == 'cm':
+		number = 1/100 
+	elif unit_name == 'mm':
+		number = 1/1000
+	elif unit_name == 'inch':
+		number=0.0254
 	else:
 		raise ValueError('unknown unit name {!r}'.format(unit_name))
 	unit_number = length_unit[1]
 	return unit_number * number
-	
 
+# this function converts sort of length units (millimeters,metres,centimetre,inch) to kilometres
+
+def kilometre(length_unit):
+	unit_name = length_unit[0].lower()
+	number = 1
+	if unit_name == 'km':
+		pass 
+	elif unit_name == 'm':
+		number = 0.001
+	elif unit_name == 'cm':
+		number = 0.00001 
+	elif unit_name == 'mm':
+		number = 0.000001
+	elif unit_name == 'inch':
+		number=0.0000254
+	else:
+		raise ValueError('unknown unit name {!r}'.format(unit_name))
+	unit_number = length_unit[1]
+	return unit_number * number
+
+# this function converts sort of length units (kilometers,metres,centimetre,millimeters) to inch
+
+def inch(length_unit):
+	unit_name = length_unit[0].lower()
+	number = 1
+	if unit_name == 'km':
+		number = 39370 
+	elif unit_name == 'm':
+		number = 39.37
+	elif unit_name == 'cm':
+		number = 0.3937 
+	elif unit_name == 'mm':
+		number = 0.03937
+	elif unit_name == 'inch':
+		pass
+	else:
+		raise ValueError('unknown unit name {!r}'.format(unit_name))
+	unit_number = length_unit[1]
+	return unit_number * number
